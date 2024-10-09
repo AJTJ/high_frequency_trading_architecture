5cf6693d7432715109fea350ab5d2aba3d2ab0ae
AJTJ <janke.aaron@gmail.com>
2024-10-09 14:49:53 +0800

    update Wed  9 Oct 2024 14:49:53 PST

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,20 @@ use exchange_v2::{
 async fn main() {
     let num_partitions = 4;
 
-    // An interesting first experiment
+    // FIRST EXPERIMENT. using mpsc
     // The problem with mpsc is that it tightly couples the sender and receive and limits scaling potential
     process_transactions_using_mpsc(num_partitions).await;
 
-    // This is ok, as it uses redis, but it doesn't use the power of consumer groups
+    // SECOND EXPERIMENT. Ok, considering it uses redis, but it doesn't use the power of consumer groups
     // It's limiting factor is that you only have one process per partition processing transactions
     if let Err(e) = process_using_redis_single_consumer(num_partitions).await {
         eprintln!("SINGLE CONSUMER with redis error: {}", e);
     }
 
-    // This is by far the best example, using redis consumer groups
+    // THIRD EXPERIMENT. This is by far the best example, using redis consumer groups
     // It would segue nicely into a Kafka setup
-    // NOTE: you would need to flush data between runs: `redis-cli FLUSHALL`
+    // NOTE: you would need to flush data between runs:
+    // `redis-cli FLUSHALL`
     if let Err(e) = process_using_redis_consumer_groups(num_partitions).await {
         eprintln!("CONSUMER GROUPS with redis error: {}", e);
     }
